997903804f99026b6457a9f3573c20febb8a4248
Laurent Burnet <lbu@nexiosit.com>
2019-10-30 09:51:31 +0100

    Added FTP constants

--- app/src/main/java/com/dentasoft/testsend/Constants.java
+++ app/src/main/java/com/dentasoft/testsend/Constants.java
@@ -1,5 +1,7 @@
 package com.dentasoft.testsend;
 
+import android.graphics.Bitmap;
+
 public class Constants {
     public static int[][] ABOUT_CUSTOMER_DETAILS = {
             {R.drawable.ic_action_name,R.string.happy_customer},
@@ -10,4 +12,18 @@ public class Constants {
             public static final String IP = "193.70.45.74";
             public static final String userName = "sms@soft4all.be";
             public static final String passWord = "@P_r6CZ#SQ*d";
+
+            public static final String ABOUT_RESOURCES_PATH = "/_webService_/about/images";
+            public static final String ABOUT_BACKGROUND_FILE = "backgroud.jpg";
+
+            public static final String HOME_SLIDER_PATH = "/_webService_/slider/img";
+
+            public static final String MENU_IMAGES_PATH = "/_webService_/config/img";
+
+            public static final String MENU_NAV_HEADER_FILE = "nav_header.png";
+
+            public static Bitmap about_image = null;
+            public static Bitmap[] slider_images = null;
+            public static String fetched = "";
+            public static Bitmap nav_header = null;
 }
